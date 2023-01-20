25e468b3f91ab3aec6b7fcbbb133735202c3377e
pedro <pedro.jose.bidoggia@hotmail.com>
2023-01-20 18:24:28 -0300

    new sections

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,31 @@
+// Nav bar menu
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+// Services Gallery
+const imgContent = document.querySelectorAll('.img-content-hover');
+function showImgContent(e) {
+    for (var i = 0; i < imgContent.length; i++) {
+        x = e.pageX;
+        y = e.pageY;
+        imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+    }
+};
+document.addEventListener('mousemove', showImgContent);
+
+// Scroll To Top
+window.addEventListener('load', function () {
+    document.getElementById('scrollTop').addEventListener('click', function () {
+        // window.scrollTo({
+        //     top: 0,
+        //     behavior: 'smooth'
+        // });
+        var position =
+            document.body.scrollTop || document.documentElement.scrollTop;
+        if (position) {
+            window.scrollBy(0, -Math.max(1, Math.floor(position / 100)));
+            scrollAnimation = setTimeout("scrollToTop()", 2000);
+        } else clearTimeout(scrollAnimation);
+    });
+});
